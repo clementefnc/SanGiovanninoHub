ab8270ead04f96d036fd0ff4566c8be5db96c151
Ausir <drakeausir@gmail.com>
2018-09-12 17:32:39 +0200

    Aggiornato pannello amministratore

--- src/amministrazione/admin.php
+++ src/amministrazione/admin.php
@@ -1,21 +1,10 @@
-<?php
-
-session_start();
-
-if($_SESSION['u_mail']!="sangiovanninolavatrici@gmail.com"){
-	header("Location: ../index_notlogged.php");
-}
-
-echo '
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SanGiovannino Hub</title>
-
+	<title>Amministrazione - SanGiovannino Hub</title>
 	<meta charset="utf-8">
 
-	<!-- FONT ROBOTO -->
-	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 
  	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
@@ -27,38 +16,81 @@ echo '
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 
 	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+
 
 </head>
-<body class="bg">
+<body style="background-color: #f9a13c">
 
-<h1>Benvenuto ADMIN</h1>
-<form action="../inc/inc_logout.php" method="post">
-    <button type="submit" >LOGOUT</button>
-  </form>
+	<div class="topnav" style="position:relative">
+	  <a class="active" href="https://www.sangiovannino.altervista.org">SanGiovannino HUB</a>
+	  <form method="post" action="../inc/inc_logout.php" style="margin:12px; padding:0">
+	  	<button type="submit" name="submit" class="btn btn-danger" style="position:absolute; right:10px;">LOGOUT</button>
+	  </form>
+	</div>
 
-Le seguenti mail ancora non sono state abilitate: <br>';
+	<div class="accordion" id="accordionExample">
+		  <div class="card">
+		    <div class="card-header" id="headingOne">
+		      <h5 class="mb-0">
+		        <button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+		          Validazione Utente
+		        </button>
+		      </h5>
+		    </div>
 
-include_once '../inc/db.inc.php';
-$sql = "SELECT users_mail, users_name, users_cog FROM users WHERE abilitato=0";
-$result = mysqli_query($conn, $sql);
-$num=$result->num_rows;
-while ($row = $result->fetch_assoc()) {  
-	$temp = $row[users_mail];
-	echo "Mail: " .$temp 
-		."<br>Nome: " . $row[users_name] . " " . $row[users_cog] 
-	. '
-	
-	<form action="inc/abilita.php" method="post">
-	<input type="text" name="mail" id="mail" value="' . $temp . '" style="display: none;">
-	<button type="submit" name="submit">Abilita</button>
-	</form>
-	
-	' . '<br><br>';
-}
+		    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+		      <div class="card-body">
+		        <?php 
+		        	include_once '../inc/db.inc.php';
+					$sql = "SELECT users_mail, users_name, users_cog, users_room FROM users WHERE abilitato=0";
+					$result = mysqli_query($conn, $sql);
+					$num=$result->num_rows;
+					while ($row = $result->fetch_assoc()) { 
 
-echo '
+						$temp = $row[users_mail];
+						echo '<div style="margin-bottom:10px"> <span style="display:inline-block">' . $row[users_name] . ' ' . $row[users_cog] . '('. $temp . ')   ' . '</span>'
+						. 
+						'<form style="margin-top:0px; display:initial; text-align:initial" action="inc/abilita.php" method="post">
+						<input type="text" name="mail" id="mail" value="' . $temp . '" style="display: none;">
+						<button type="submit" class="btn btn-success" name="abilita">Abilita</button>
+						<button type="submit" class="btn btn-danger" name="cancella">Cancella</button>
+						</form> <br> </div>';
+					}
+
+		        ?>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <div class="card-header" id="headingTwo">
+		      <h5 class="mb-0">
+		        <button class="btn btn-warning collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+		          Varia Stanza
+		        </button>
+		      </h5>
+		    </div>
+		    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+		      <div class="card-body">
+		        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		      </div>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <div class="card-header" id="headingThree">
+		      <h5 class="mb-0">
+		        <button class="btn btn-primary collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+		          Kick
+		        </button>
+		      </h5>
+		    </div>
+		    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+		      <div class="card-body">
+		        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
 </body>
-</html>';
-
-?>
+</html>
--- src/amministrazione/inc/abilita.php
+++ src/amministrazione/inc/abilita.php
@@ -1,6 +1,6 @@
 <?php 
 
-if( isset($_POST['submit']) ) {
+if( isset($_POST['abilita']) ) {
 
     include_once '../../inc/db.inc.php';
 
@@ -12,6 +12,20 @@ if( isset($_POST['submit']) ) {
     }
     else{
         mysqli_query($conn, "UPDATE users SET abilitato=1 WHERE users_mail='$mail'");
+    }
+}
+
+else if ( isset($_POST['cancella']) ) {
+	include_once '../../inc/db.inc.php';
+
+    $mail = mysqli_real_escape_string($conn, $_POST['mail']);
+
+    if(empty($mail)){
+        header("Location: ../admin.php?error=invalid");
+        exit();
+    }
+    else{
+        mysqli_query($conn, "DELETE FROM users WHERE users_mail='$mail'");
     }
 }
 
