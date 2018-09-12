fc6e05ed4a7ac908fadd03f94a1c585e7e53c5f8
Ausir <drakeausir@gmail.com>
2018-09-12 16:07:01 +0200

    Inserito limite prenotazioni giornaliere

--- src/inc/inc_prenota.php
+++ src/inc/inc_prenota.php
@@ -28,10 +28,31 @@ if(isset($_POST['submit'])){
             echo '<script type="text/javascript">alert("ORA GIÀ PRENOTATA."); window.location="../lavasciuga/indexLavasciuga.php"</script>';
         }
         else{
-            $sql = "INSERT INTO prenotazioni (email,giorno,mese,anno,ora) 
-                VALUES (" . "'" . $_SESSION['u_mail'] . "'" . "," . $giorno . "," . $mese . "," . $anno . "," . $ora .")";
-            mysqli_query($conn, $sql);
-            header("Location: ../lavasciuga/indexLavasciuga.php");
+
+            $sql = "SELECT COUNT(*) AS cnt
+                    FROM prenotazioni 
+                    WHERE email=" . "'" . $_SESSION['u_mail'] . "'" . 
+                    " AND giorno=" . $giorno .
+                    " AND mese=" . $mese . 
+                    " AND anno=" . $anno;
+
+            $result = mysqli_query($conn, $sql);
+
+            $check = mysqli_num_rows($result);
+
+                    if($check>0)
+                        $row = $result->fetch_assoc();
+                
+
+            if (intval($row['cnt'],10) >= 2) {
+                echo '<script type="text/javascript">alert("NUMERO MASSIMO DI PRENOTAZIONI RAGGIUNTO."); window.location="../lavasciuga/indexLavasciuga.php"</script>';
+            }else{
+
+                $sql = "INSERT INTO prenotazioni (email,giorno,mese,anno,ora) 
+                    VALUES (" . "'" . $_SESSION['u_mail'] . "'" . "," . $giorno . "," . $mese . "," . $anno . "," . $ora .")";
+                mysqli_query($conn, $sql);
+                header("Location: ../lavasciuga/indexLavasciuga.php");
+            }
         }
 
     }else{
@@ -49,10 +70,31 @@ if(isset($_POST['submit'])){
                 echo '<script type="text/javascript">alert("ORA GIÀ PRENOTATA."); window.location="../lavasciuga/indexLavasciuga.php"</script>';
             }
             else{
-                $sql = "INSERT INTO asciugatrice (a_email,a_giorno,a_mese,a_anno,a_ora) 
-                VALUES (" . "'" . $_SESSION['u_mail'] . "'" . "," . $giorno . "," . $mese . "," . $anno . "," . $ora .")";
-            mysqli_query($conn, $sql);
-            header("Location: ../lavasciuga/indexLavasciuga.php");
+
+                 $sql = "SELECT COUNT(*) AS cnt
+                    FROM asciugatrice 
+                    WHERE a_email=" . "'" . $_SESSION['u_mail'] . "'" . 
+                    " AND a_giorno=" . $giorno .
+                    " AND a_mese=" . $mese . 
+                    " AND a_anno=" . $anno;
+
+            $result = mysqli_query($conn, $sql);
+
+            $check = mysqli_num_rows($result);
+
+                    if($check>0)
+                        $row = $result->fetch_assoc();
+                
+
+                if (intval($row['cnt'],10) >= 2) {
+                    echo '<script type="text/javascript">alert("NUMERO MASSIMO DI PRENOTAZIONI RAGGIUNTO."); window.location="../lavasciuga/indexLavasciuga.php"</script>';
+                }else{
+
+                    $sql = "INSERT INTO asciugatrice (a_email,a_giorno,a_mese,a_anno,a_ora) 
+                    VALUES (" . "'" . $_SESSION['u_mail'] . "'" . "," . $giorno . "," . $mese . "," . $anno . "," . $ora .")";
+                mysqli_query($conn, $sql);
+                header("Location: ../lavasciuga/indexLavasciuga.php");
+                }
             }
     }
 
