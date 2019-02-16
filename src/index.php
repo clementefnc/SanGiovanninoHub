ccca46a1ff313c8f837ebbf9c731dbc137940adf
clementefnc <clementefnc@gmail.com>
2019-02-16 23:32:30 +0100

    Creati header, footer, navbar

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+
+<body class="bg-secondary">
+	
+	<?php include 'navbar.php'; ?>
+
+	<div style="padding: 0; margin: 0;" align="center">
+
+		<h2 style="font-family: Roboto, sans-serif; color:white; margin-top: 30px; margin-bottom:0;">Benvenuti su</h2>
+		<img src="img/logo.png" style="width:600px; height: 600px; margin-top: -70px;"></img>
+
+	</div>
+
+</body>
+
+<?php include 'footer.php'; ?>
